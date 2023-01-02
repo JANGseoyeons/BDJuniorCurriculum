537af3d03fd80365ffdae9f32c71a0aa40caa013
seoyeon <tjsus247@naver.com>
2023-01-02 14:51:07 +0900

    Quest05 checklist

--- Quest04/skeleton/desktop.js
+++ Quest04/skeleton/desktop.js
@@ -4,23 +4,24 @@ class Desktop {
   constructor(props) {
     this.props = props;
   }
-  folder() {
-    let div = document.createElement("div");
-    div.setAttribute("class", "folder");
-    this.props.appendChild(div);
-    return this.props;
-  }
 }
 
 class Icon {
   /* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+  //드래그 기능
+  drag() {
+    console.log("hi");
+  }
 }
 
-class Folder {
+class Folder extends Icon {
   /* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-  // 폴더 아이콘의 위치
+  // 더블 클릭 시 폴더 창열림 기능
+  open() {}
 }
 
 class Window {
   /* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+  // 아이콘, 폴더 갯수 기능
+  count() {}
 }
