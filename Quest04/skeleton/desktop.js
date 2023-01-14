6b654ba77fb21301c2e0683c590718474585e059
seoyeon <tjsus247@naver.com>
2023-01-14 17:24:58 +0900

    Quest04

--- Quest04/skeleton/desktop.js
+++ Quest04/skeleton/desktop.js
@@ -1,29 +1,112 @@
 class Desktop {
   /* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-  // desktop html 가져옴
-  constructor(props) {
-    this.props = props;
+  rander() {
+    // 입력 창 생성, 입력한 숫자를 받아와 숫자에 맞게 출력
+    let myWindow = new Window();
   }
 }
-
 class Icon {
-  /* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-  constructor(isClicked, offsetX, offsetY, currentObject) {
-    this.isClicked = isClicked;
-    this.offsetX = offsetX;
-    this.offsetY = offsetY;
-    this.currentObject = currentObject;
+  /* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요?  
+  요소생성 : 아이콘 (innerHtml) 
+  기능 : 드래그 */
+  constructor(id) {
+    this.div = document.createElement("div");
+    this.div.id = id;
+    this.section = document.querySelector(".desktop");
+    this.isDragging = false;
+    this.currentX;
+    this.currentY;
+    this.initialX;
+    this.initialY;
+    this.xOffset = 0;
+    this.yOffset = 0;
+
+    this.div.addEventListener("mousedown", this.dragStart.bind(this));
+    this.div.addEventListener("mouseup", this.dragEnd.bind(this));
+    this.div.addEventListener("mousemove", this.drag.bind(this));
+    this.section.append(this.div);
+  }
+
+  dragStart(event) {
+    this.initialX = event.clientX - this.xOffset;
+    this.initialY = event.clientY - this.yOffset;
+
+    if (event.target === this.div) {
+      this.isDragging = true;
+    }
+  }
+
+  dragEnd(event) {
+    this.initialX = this.currentX;
+    this.initialY = this.currentY;
+
+    this.isDragging = false;
+  }
+
+  drag(event) {
+    if (this.isDragging) {
+      event.preventDefault();
+      this.currentX = event.clientX - this.initialX;
+      this.currentY = event.clientY - this.initialY;
+
+      this.xOffset = this.currentX;
+      this.yOffset = this.currentY;
+
+      this.setTranslate(this.currentX, this.currentY, this.div);
+    }
+  }
+
+  setTranslate(xPos, yPos, el) {
+    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
   }
 }
-
+// 드래그 기능(Icon 상속)
 class Folder extends Icon {
   /* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-  // 더블 클릭 시 폴더 창열림 기능
-  open() {}
+  constructor(id) {
+    super(id);
+  }
+  // 폴더창 열기 기능
+  open() {
+    let folder = new Folder("model");
+    // this.model = document.createElement("div");
+    // this.model.className = "model";
+    // this.model.innerHTML = `<p>폴더창</p><button class='close'>닫기</button>`;
+    // const section = document.querySelector(".desktop");
+    // section.appendChild(this.model);
+    // this.model.addEventListener("mousedown", Icon.dragStart);
+    // this.model.addEventListener("mouse", Icon.drag);
+    // this.model.addEventListener("mouseup", Icon.dragEnd);
+  }
+
+  rander() {
+    this.div.addEventListener("dblclick", this.open);
+  }
 }
 
 class Window {
   /* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-  // 아이콘, 폴더 갯수 기능
-  count() {}
+  // 아이콘과 폴더 갯수를 받아 그 갯수 만큼 출력 코드
+  constructor() {
+    this.input1 = document.createElement("input");
+    this.input2 = document.createElement("input");
+    this.input1.className = "IconInput";
+    this.input2.className = "FolderInput";
+    const section = document.querySelector(".desktop");
+    section.innerHTML = `<button class="btn">전송</button>`;
+    section.append(this.input1, this.input2);
+    const btn = document.querySelector(".btn");
+    this.d1 = document.querySelector(".IconInput");
+    this.d2 = document.querySelector(".FolderInput");
+    btn.addEventListener("click", this.post.bind(this));
+  }
+  post() {
+    for (let i = 0; i < this.input1.value; i++) {
+      let myIcon = new Icon("Icon", this.d1.value, this.d2.value);
+    }
+    for (let i = 0; i < this.input2.value; i++) {
+      let myFolder = new Folder("Folder");
+      myFolder.rander();
+    }
+  }
 }
--- Quest08/README.md
+++ Quest08/README.md
@@ -24,20 +24,39 @@
   get으로 서버에게 동일한 요청을 여러 번 전송하더라도 동일한 응답이 돌아와햐 한다는 것을 의미하며,  
   post는 서버에게 동일한 요청을 여러 번 전송해도 응답은 항상 다를 수 있습니다.
 
-- 다른 HTTP 메소드에는 무엇이 있나요?
+- 다른 HTTP 메소드에는 무엇이 있나요?  
+  HEAD : 메세지 헤더(문서 정보) 취득
+  GET : URL 형식으로 웹 서버측 리소스(데이터)를 요청한다.(보안에 취약)
+  POST : 특정 리소스를 엔티티를 제출 (내용 전송) -> get메소드보다 보안에 안정
+  PUT : 목적 리소스 모든 현재 표시를 요청 palyoad로 바꾼다. -> 내용 갱신 위주(파일 전송 가능)
+  PATCH : 리소스의 부분만을 수정하는데 쓰인다.
+  DELETE : 파일을 삭제한다.
+  OPTIONS : 대상 리소스에 대한 통신 가능 옵션(메소드)을 설명
+  CONNECT : 대상 자원으로 식별되는 서버에 대한 터널을 설정
+  TRACE : 대상 리소스에 대한 경로를 따라 메시지 루프백 테스트를 수행
+
 - HTTP 서버에 GET과 POST를 통해 데이터를 보내려면 어떻게 해야 하나요?
+  GET 방식은 URL에 Parameter를 붙여서 전송한다.(데이터가 담기는 곳 : header)
+  POST 방식은 GET 방식과는 달리 body영역에 데이터를 실어 보낸다는 점이다. GET방식과는 달리 데이터를 URL를 통해 볼 수 없어 보안적이다.(데이터가 담기는 곳 : body)
 
   - HTTP 요청의 `Content-Type` 헤더는 무엇인가요?  
     Content-Type은 보내는 자원의 형태를 명시하는 헤더입니다.  
     content-Type의 종류는 Multipart Related MIME타입, XML Media타입, Application 타입, 오디오 타입, Multipart타입, TEXT타입이 있다.
 
-  - Postman에서 POST 요청을 보내는 여러 가지 방법(`form-data`, `x-www-form-urlencoded`, `raw`, `binary`) 각각은 어떤 용도를 가지고 있나요?
+  - Postman에서 POST 요청을 보내는 여러 가지 방법(`form-data`, `x-www-form-urlencoded`, `raw`, `binary`) 각각은 어떤 용도를 가지고 있나요?  
+    Form Data(양식 데이터) : 양식을 채울 때 입력 한 세부 정보와 같이 양숙 내부의 래핑하는 데이터를 보내는데 사용(Key-Value 쌍)
+    x-www-form-urlencoded : url이 x-www-form-urlencoded를 통해 전송 될 때 인코딩된다.(인코딩 : 전송되는 데이터가 다른 문자로 인코딩되어 공격을 받고 있어도 인식할 수 없음을 의미)
+    Raw : 본문 메시지가 요청 본문을 나타내는 비트 스트림으로 표시됨을 의미 -> 문자열 서버로 해석됨
+    Binary : 수동으로 입력할 수 없는 형식으로 데이터를 전송되도록 설계
 
-- node.js의 `http` 모듈을 통해 HTTP 요청을 처리할 때,
+- node.js의 `http` 모듈을 통해 HTTP 요청을 처리할 때, `req`와 `res` 객체에는 어떤 정보가 담겨있을까요?  
+   req : 클라이언트가 서버로 전달해서 서버의 액션이 일어나게끔하는 메시지이다.
+  method, path, version of protocol, headers, body
+  res : 요청에 대한 서버의 답변이다.
+  version of protocol, status code, status message, headers, body
 
-  - `req`와 `res` 객체에는 어떤 정보가 담겨있을까요? ->
-
-  - GET과 POST에 대한 처리 형태가 달라지는 이유는 무엇인가요? -> 기본적으로 get과 post가 참조하는 기반이 다르며, get이 read하기 위한 목적, post는 update한 정보는 create하기 위한 목적이다.
+  - GET과 POST에 대한 처리 형태가 달라지는 이유는 무엇인가요?  
+    기본적으로 get과 post가 참조하는 기반이 다르며, get이 read하기 위한 목적, post는 update한 정보는 create하기 위한 목적이다.
 
 - 만약 API 엔드포인트(URL)가 아주 많다고 한다면, HTTP POST 요청의 `Content-Type` 헤더에 따라 다른 방식으로 동작하는 서버를 어떻게 정리하면 좋을까요?->
 
