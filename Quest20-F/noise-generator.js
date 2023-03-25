d694e4a83632f797974daf1cda5acbfa72f7fec2
seoyeon <tjsus247@naver.com>
2023-03-25 11:31:42 +0900

    Quest20 완성

--- Quest20-F/noise-generator.js
+++ Quest20-F/noise-generator.js
@@ -4,13 +4,17 @@
  * @class NoiseGenerator
  * @extends AudioWorkletProcessor
  */
+
+// AudioWorkletProcessor상속함으로서 NoiseGenerator 클래스는 오디오 처리를 위한 process() 메서드를 구현이 가능
 class NoiseGenerator extends AudioWorkletProcessor {
+  // 노드에서 사용할 AudioParam(노이즈 크기 조절) 객체들의 목록을 정의를 함
   static get parameterDescriptors() {
     return [
       { name: "amplitude", defaultValue: 0.25, minValue: 0, maxValue: 1 },
     ];
   }
 
+  // 오디오 처리를 수행
   process(inputs, outputs, parameters) {
     const output = outputs[0];
     const amplitude = parameters.amplitude;
